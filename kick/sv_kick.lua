1ca724586783ba02a340a152e7dfd98b5eb7715f
Nathan C <me@nathanc.tech>
2020-07-15 14:28:08 -0400

    Initial commit by community author

---
+++ kick/sv_kick.lua
@@ -0,0 +1,17 @@
+--[[
+    Sonaran CAD Plugins
+
+    Plugin Name: kick
+    Creator: Taylor McGaw
+    Description: Kicks user from the cad upon exiting the server
+]]
+
+local pluginConfig = Config.GetPluginConfig("kick")
+
+registerApiType("KICK_UNIT", "emergency")
+    AddEventHandler("playerDropped", function()
+        local source = source
+        local identifier = GetIdentifiers(source)[Config.primaryIdentifier]
+        local data = {['apiId'] = identifier, ['reason'] = "You have Exited the server"}
+        performApiRequest({data}, 'KICK_UNIT', function() end)
+    end)
