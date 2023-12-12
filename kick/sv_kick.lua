6d464451a3aea4b47a3844eceb5b0ac01470bc36
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2023-12-12 14:45:28 -0500

    Refactor kick plugin to handle multiple API IDs

--- kick/sv_kick.lua
+++ kick/sv_kick.lua
@@ -1,53 +1,63 @@
---[[
-    Sonaran CAD Plugins
+    --[[
+        Sonaran CAD Plugins
 
-    Plugin Name: kick
-    Creator: Taylor McGaw
-    Description: Kicks user from the cad upon exiting the server
-]]
+        Plugin Name: kick
+        Creator: Taylor McGaw
+        Description: Kicks user from the cad upon exiting the server
+    ]]
+    local pluginConfig = Config.GetPluginConfig('kick')
 
-local pluginConfig = Config.GetPluginConfig("kick")
+    if pluginConfig.enabled then
 
-if pluginConfig.enabled then
-
-    local PendingKicks = {}
-    registerApiType("KICK_UNIT", "emergency")
-    AddEventHandler("playerDropped", function()
-        local source = source
-        local identifier = GetIdentifiers(source)[Config.primaryIdentifier]
-        if not identifier then
-            debugLog("kick: no API ID, skip")
-            return
-        end
-        performApiRequest({
-            ['serverId'] = Config.serverId,
-            ['onlyUnits'] = true,
-        }, "GET_ACTIVE_UNITS", function(units)
-            if units ~= nil and #units > 0 then
-                for _, unit in pairs(units) do
-                    if unit.data.apiId1 == identifier or unit.data.apiId2 == identifier then
-                        table.insert(PendingKicks, identifier)
+        local PendingKicks = {}
+        registerApiType('KICK_UNIT', 'emergency')
+        AddEventHandler('playerDropped', function()
+            local source = source
+            local identifier = GetIdentifiers(source)[Config.primaryIdentifier]
+            if not identifier then
+                debugLog('kick: no API ID, skip')
+                return
+            end
+            performApiRequest({
+                {
+                    ['serverId'] = Config.serverId,
+                    ['onlyUnits'] = true
+                }
+            }, 'GET_ACTIVE_UNITS', function(units)
+                units = json.decode(units)
+                if units ~= nil and #units > 0 then
+                    for _, unit in pairs(units) do
+                        for _, apiId in ipairs(unit.data.apiIds) do
+                            if apiId == identifier then
+                                table.insert(PendingKicks, identifier)
+                                break -- exit the loop once the identifier is found
+                            end
+                        end
                     end
                 end
+            end)
+        end)
+
+        CreateThread(function()
+            while true do
+                if #PendingKicks > 0 then
+                    local kicks = {}
+                    while true do
+                        local pendingKick = table.remove(PendingKicks)
+                        if pendingKick ~= nil then
+                            table.insert(kicks, {
+                                ['apiId'] = pendingKick,
+                                ['reason'] = 'You have exited the server',
+                                ['serverId'] = Config.serverId
+                            })
+                        else
+                            break
+                        end
+                    end
+                    performApiRequest(kicks, 'KICK_UNIT', function()
+                    end)
+                end
+                Wait(10000)
             end
         end)
-    end)
-
-    CreateThread(function()
-        while true do
-            if #PendingKicks > 0 then
-                local kicks = {}
-                while true do
-                    local pendingKick = table.remove(PendingKicks)
-                    if pendingKick ~= nil then
-                        table.insert(kicks, {["apiId"] = pendingKick, ["reason"] = "You have exited the server", ["serverId"] = Config.serverId})
-                    else
-                        break
-                    end
-                end
-                performApiRequest(kicks, 'KICK_UNIT', function() end)
-            end
-            Wait(10000)
-        end
-    end)
-end
+    end
